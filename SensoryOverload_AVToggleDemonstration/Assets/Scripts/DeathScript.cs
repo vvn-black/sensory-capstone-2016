ba9f74f0b59ad474fe27908cbbec032959a47450
Cody Black <cody.black.47@gmail.com>
2016-09-15 19:21:25 -0600

    I did some reorganization of folders, deleted a bunch of stuff that we shouldn't need in the project, and added the ability to die by walking off the edge. I should have also set up the .gitignore correctly so that we don't have thousands of files changing with every push.

---
+++ SensoryOverload_AVToggleDemonstration/Assets/Scripts/DeathScript.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using UnityStandardAssets.Characters.FirstPerson;
+
+public class DeathScript : MonoBehaviour {
+    public GameObject playerObject;
+    private RigidbodyFirstPersonController playerScript;
+
+    public void Start()
+    {
+        playerScript = playerObject.GetComponent<RigidbodyFirstPersonController>();
+    }
+
+    public void KillPlayer()
+    {
+        playerObject.transform.position = new Vector3(playerScript.startingLocation.x, playerScript.startingLocation.y, playerScript.startingLocation.z);
+    }
+}
--- SensoryOverload_AVToggleDemonstration/Assets/Standard Assets/Characters/FirstPersonCharacter/Scripts/RigidbodyFirstPersonController.cs
+++ SensoryOverload_AVToggleDemonstration/Assets/Standard Assets/Characters/FirstPersonCharacter/Scripts/RigidbodyFirstPersonController.cs
@@ -6,11 +6,14 @@ namespace UnityStandardAssets.Characters.FirstPerson
 {
     [RequireComponent(typeof (Rigidbody))]
     [RequireComponent(typeof (CapsuleCollider))]
-    public class RigidbodyFirstPersonController : MonoBehaviour
-    {
+    public class RigidbodyFirstPersonController : MonoBehaviour{
         [Serializable]
-        public class MovementSettings
-        {
+        public class StartingLocation {
+            public float x, y, z = 0.0f;
+        }
+
+        [Serializable]
+        public class MovementSettings {
             public float ForwardSpeed = 8.0f;   // Speed when walking forward
             public float BackwardSpeed = 4.0f;  // Speed when walking backwards
             public float StrafeSpeed = 4.0f;    // Speed when walking sideways
@@ -24,35 +27,22 @@ namespace UnityStandardAssets.Characters.FirstPerson
             private bool m_Running;
 #endif
 
-            public void UpdateDesiredTargetSpeed(Vector2 input)
-            {
+            public void UpdateDesiredTargetSpeed(Vector2 input) {
 	            if (input == Vector2.zero) return;
-				if (input.x > 0 || input.x < 0)
-				{
-					//strafe
-					CurrentTargetSpeed = StrafeSpeed;
-				}
-				if (input.y < 0)
-				{
-					//backwards
+				if (input.x > 0 || input.x < 0) //strafe
+                    CurrentTargetSpeed = StrafeSpeed;
+				if (input.y < 0) //backwards
 					CurrentTargetSpeed = BackwardSpeed;
-				}
-				if (input.y > 0)
-				{
-					//forwards
-					//handled last as if strafing and moving forward at the same time forwards speed should take precedence
-					CurrentTargetSpeed = ForwardSpeed;
-				}
+				if (input.y > 0) //forwards
+					CurrentTargetSpeed = ForwardSpeed; //handled last, as if strafing and moving forward at the same time, forward speed should take precedence
+
 #if !MOBILE_INPUT
-	            if (Input.GetKey(RunKey))
-	            {
+	            if (Input.GetKey(RunKey)) {
 		            CurrentTargetSpeed *= RunMultiplier;
 		            m_Running = true;
 	            }
 	            else
-	            {
 		            m_Running = false;
-	            }
 #endif
             }
 
@@ -66,8 +56,7 @@ namespace UnityStandardAssets.Characters.FirstPerson
 
 
         [Serializable]
-        public class AdvancedSettings
-        {
+        public class AdvancedSettings {
             public float groundCheckDistance = 0.01f; // distance for checking if the controller is grounded ( 0.01f seems to work best for this )
             public float stickToGroundHelperDistance = 0.5f; // stops the character
             public float slowDownRate = 20f; // rate at which the controller comes to a stop when there is no input
@@ -78,10 +67,10 @@ namespace UnityStandardAssets.Characters.FirstPerson
 
 
         public Camera cam;
+        public StartingLocation startingLocation = new StartingLocation();
         public MovementSettings movementSettings = new MovementSettings();
         public MouseLook mouseLook = new MouseLook();
         public AdvancedSettings advancedSettings = new AdvancedSettings();
-
 
         private Rigidbody m_RigidBody;
         private CapsuleCollider m_Capsule;
@@ -90,25 +79,20 @@ namespace UnityStandardAssets.Characters.FirstPerson
         private bool m_Jump, m_PreviouslyGrounded, m_Jumping, m_IsGrounded;
 
 
-        public Vector3 Velocity
-        {
+        public Vector3 Velocity {
             get { return m_RigidBody.velocity; }
         }
 
-        public bool Grounded
-        {
+        public bool Grounded {
             get { return m_IsGrounded; }
         }
 
-        public bool Jumping
-        {
+        public bool Jumping {
             get { return m_Jumping; }
         }
 
-        public bool Running
-        {
-            get
-            {
+        public bool Running {
+            get {
  #if !MOBILE_INPUT
 				return movementSettings.Running;
 #else
@@ -118,112 +102,87 @@ namespace UnityStandardAssets.Characters.FirstPerson
         }
 
 
-        private void Start()
-        {
+        private void Start() {
             m_RigidBody = GetComponent<Rigidbody>();
             m_Capsule = GetComponent<CapsuleCollider>();
+            transform.position = new Vector3(startingLocation.x, startingLocation.y, startingLocation.z);
             mouseLook.Init (transform, cam.transform);
         }
 
 
-        private void Update()
-        {
+        private void Update() {
             RotateView();
-
-            if (CrossPlatformInputManager.GetButtonDown("Jump") && !m_Jump)
-            {
+            if (CrossPlatformInputManager.GetButtonDown("Jump") && !m_Jump) {
                 m_Jump = true;
             }
         }
 
 
-        private void FixedUpdate()
-        {
+        private void FixedUpdate() {
             GroundCheck();
             Vector2 input = GetInput();
 
-            if ((Mathf.Abs(input.x) > float.Epsilon || Mathf.Abs(input.y) > float.Epsilon) && (advancedSettings.airControl || m_IsGrounded))
-            {
+            if ((Mathf.Abs(input.x) > float.Epsilon || Mathf.Abs(input.y) > float.Epsilon) && (advancedSettings.airControl || m_IsGrounded)) {
                 // always move along the camera forward as it is the direction that it being aimed at
                 Vector3 desiredMove = cam.transform.forward*input.y + cam.transform.right*input.x;
                 desiredMove = Vector3.ProjectOnPlane(desiredMove, m_GroundContactNormal).normalized;
-
                 desiredMove.x = desiredMove.x*movementSettings.CurrentTargetSpeed;
                 desiredMove.z = desiredMove.z*movementSettings.CurrentTargetSpeed;
                 desiredMove.y = desiredMove.y*movementSettings.CurrentTargetSpeed;
                 if (m_RigidBody.velocity.sqrMagnitude <
-                    (movementSettings.CurrentTargetSpeed*movementSettings.CurrentTargetSpeed))
-                {
+                    (movementSettings.CurrentTargetSpeed*movementSettings.CurrentTargetSpeed)) {
                     m_RigidBody.AddForce(desiredMove*SlopeMultiplier(), ForceMode.Impulse);
                 }
             }
 
-            if (m_IsGrounded)
-            {
+            if (m_IsGrounded) {
                 m_RigidBody.drag = 5f;
 
-                if (m_Jump)
-                {
+                if (m_Jump) {
                     m_RigidBody.drag = 0f;
                     m_RigidBody.velocity = new Vector3(m_RigidBody.velocity.x, 0f, m_RigidBody.velocity.z);
                     m_RigidBody.AddForce(new Vector3(0f, movementSettings.JumpForce, 0f), ForceMode.Impulse);
                     m_Jumping = true;
                 }
-
                 if (!m_Jumping && Mathf.Abs(input.x) < float.Epsilon && Mathf.Abs(input.y) < float.Epsilon && m_RigidBody.velocity.magnitude < 1f)
-                {
                     m_RigidBody.Sleep();
-                }
             }
-            else
-            {
+            else {
                 m_RigidBody.drag = 0f;
                 if (m_PreviouslyGrounded && !m_Jumping)
-                {
                     StickToGroundHelper();
-                }
             }
             m_Jump = false;
         }
 
 
-        private float SlopeMultiplier()
-        {
+        private float SlopeMultiplier() {
             float angle = Vector3.Angle(m_GroundContactNormal, Vector3.up);
             return movementSettings.SlopeCurveModifier.Evaluate(angle);
         }
 
 
-        private void StickToGroundHelper()
-        {
+        private void StickToGroundHelper() {
             RaycastHit hitInfo;
             if (Physics.SphereCast(transform.position, m_Capsule.radius * (1.0f - advancedSettings.shellOffset), Vector3.down, out hitInfo,
                                    ((m_Capsule.height/2f) - m_Capsule.radius) +
                                    advancedSettings.stickToGroundHelperDistance, ~0, QueryTriggerInteraction.Ignore))
-            {
                 if (Mathf.Abs(Vector3.Angle(hitInfo.normal, Vector3.up)) < 85f)
-                {
                     m_RigidBody.velocity = Vector3.ProjectOnPlane(m_RigidBody.velocity, hitInfo.normal);
-                }
-            }
         }
 
 
-        private Vector2 GetInput()
-        {
-            
-            Vector2 input = new Vector2
-                {
-                    x = CrossPlatformInputManager.GetAxis("Horizontal"),
-                    y = CrossPlatformInputManager.GetAxis("Vertical")
-                };
+        private Vector2 GetInput() {
+            Vector2 input = new Vector2 {
+                x = CrossPlatformInputManager.GetAxis("Horizontal"),
+                y = CrossPlatformInputManager.GetAxis("Vertical")
+            };
 			movementSettings.UpdateDesiredTargetSpeed(input);
             return input;
         }
 
 
-        private void RotateView()
-        {
+        private void RotateView() {
             //avoids the mouse looking if the game is effectively paused
             if (Mathf.Abs(Time.timeScale) < float.Epsilon) return;
 
@@ -232,8 +191,7 @@ namespace UnityStandardAssets.Characters.FirstPerson
 
             mouseLook.LookRotation (transform, cam.transform);
 
-            if (m_IsGrounded || advancedSettings.airControl)
-            {
+            if (m_IsGrounded || advancedSettings.airControl) {
                 // Rotate the rigidbody velocity to match the new direction that the character is looking
                 Quaternion velRotation = Quaternion.AngleAxis(transform.eulerAngles.y - oldYRotation, Vector3.up);
                 m_RigidBody.velocity = velRotation*m_RigidBody.velocity;
@@ -241,25 +199,20 @@ namespace UnityStandardAssets.Characters.FirstPerson
         }
 
         /// sphere cast down just beyond the bottom of the capsule to see if the capsule is colliding round the bottom
-        private void GroundCheck()
-        {
+        private void GroundCheck() {
             m_PreviouslyGrounded = m_IsGrounded;
             RaycastHit hitInfo;
             if (Physics.SphereCast(transform.position, m_Capsule.radius * (1.0f - advancedSettings.shellOffset), Vector3.down, out hitInfo,
-                                   ((m_Capsule.height/2f) - m_Capsule.radius) + advancedSettings.groundCheckDistance, ~0, QueryTriggerInteraction.Ignore))
-            {
+                                   ((m_Capsule.height/2f) - m_Capsule.radius) + advancedSettings.groundCheckDistance, ~0, QueryTriggerInteraction.Ignore)) {
                 m_IsGrounded = true;
                 m_GroundContactNormal = hitInfo.normal;
             }
-            else
-            {
+            else {
                 m_IsGrounded = false;
                 m_GroundContactNormal = Vector3.up;
             }
             if (!m_PreviouslyGrounded && m_IsGrounded && m_Jumping)
-            {
                 m_Jumping = false;
-            }
         }
     }
 }
