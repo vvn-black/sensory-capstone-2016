01aed697b55478822595c4a146c2cf5cd2966bb2
Cody Black <cody.black.47@gmail.com>
2016-09-17 02:50:31 -0600

    Rebuilt (generated .meta files for) the project from scratch. Luckily it appears the new .gitignore is actually ignoring the correct files now, so the project should be smaller, and this time we should have all the .meta files synced between everyone. I will test this on my laptop to make sure it actually works.

---
+++ SensoryOverload_AVToggleDemonstration/Assets/Scripts/DeathScript.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using UnityStandardAssets.Characters.FirstPerson;
+
+public class DeathScript : MonoBehaviour {
+    public GameObject playerObject;
+    private RigidbodyFirstPersonController playerScript;
+    private GlobalTimer timer;
+
+    public void Start()
+    {
+        playerScript = playerObject.GetComponent<RigidbodyFirstPersonController>();
+        timer = GetComponentInParent<GlobalTimer>();
+    }
+
+    void Update()
+    {
+        if (timer.currentColor == GlobalTimer.LightColor.Red && 
+            (playerScript.Velocity.magnitude > 0.05 || playerScript.Velocity.magnitude < -0.05))
+            KillPlayer();
+    }
+
+    public void KillPlayer()
+    {
+        playerObject.transform.position = new Vector3(playerScript.startingLocation.x, playerScript.startingLocation.y, playerScript.startingLocation.z);
+        playerObject.transform.eulerAngles = new Vector3(0, playerScript.startingLocation.rotation, 0);
+        playerScript.mouseLook.Init(playerObject.transform, playerScript.cam.transform);
+        timer.ResetVisualTimer();
+    }
+}
---
+++ SensoryOverload_AVToggleDemonstration/Assets/Scripts/DeathTrigger.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DeathTrigger : MonoBehaviour {
+    private DeathScript deathScript;
+
+    void Start()
+    {
+        deathScript = GetComponentInParent<DeathScript>();
+    }
+
+    void OnTriggerEnter(Collider col)
+    {
+        if(col.tag == "Player")
+        {
+            deathScript.KillPlayer();
+        }
+    }
+}
